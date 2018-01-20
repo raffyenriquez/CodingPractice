61db86ec2c776f11e32ee020e1bafcc01ec5c7df
raffyenriquez <30461868+raffyenriquez@users.noreply.github.com>
2018-01-21 00:20:38 +0800

    Create Day 0 Mean Median Mode.py

---
+++ hackerrank/10 Days of Statistics/Day 0 Mean Median Mode.py
@@ -0,0 +1,30 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+"""Compute mean, median and mode"""
+
+n = raw_input()
+array = raw_input()
+integers = []
+
+for i in array.split(" "):
+    integers.append(int(i))
+    
+#mean
+mean = sum(integers) / float(n)
+print mean
+
+#median
+sorted_integers = sorted(integers)
+middle = [sorted_integers[int(n)/2], sorted_integers[int(n)/2 -1]]
+median = sum(middle) / 2.0
+print median
+
+#mode
+integer, freq = 0, 0
+for i in sorted_integers:
+    count = sorted_integers.count(i)
+    if count > freq:
+        integer = i
+        freq = count
+mode = integer
+print mode
