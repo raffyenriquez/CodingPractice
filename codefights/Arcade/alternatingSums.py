dd68cc5f264c7691b1f215348df04d312bbb89fd
raffyenriquez <30461868+raffyenriquez@users.noreply.github.com>
2018-01-22 13:43:45 +0800

    Update alternatingSums.py

--- codefights/Arcade/alternatingSums.py
+++ codefights/Arcade/alternatingSums.py
@@ -1,5 +1,3 @@
 def alternatingSums(a):
-    """returns an array of two integers each the sum of alternating elements in an array"""
-    t1 = sum(a[::2])
-    t2 = sum(a[1::2])
-    return [t1, t2]
+    """returns an array of two integers each teh sum of alternating elements in an array"""
+    return [sum(a[::2]), sum(a[1::2])]
