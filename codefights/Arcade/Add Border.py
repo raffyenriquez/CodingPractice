72873ccf5579692cd2e550f1086421164dc16c5d
raffyenriquez <30461868+raffyenriquez@users.noreply.github.com>
2018-01-22 13:42:23 +0800

    Update Add Border.py

--- codefights/Arcade/Add Border.py
+++ codefights/Arcade/Add Border.py
@@ -1,9 +1,4 @@
 def addBorder(picture):
     """adds a border around a matrix of characters"""
-    with_border = []
-    border = (len(picture[0]) + 2) * "*"
-    with_border.append(border)
-    for i in picture:
-        with_border.append("*" + i + "*")
-    with_border.append(border)
-    return with_border
+    l = (len(picture[0]) + 2)
+    return ["*" * l] + ["*" + x + "*" for x in picture] + ["*" * l]
