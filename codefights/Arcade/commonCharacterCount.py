e590a0cff41375b854a2064b59c6c445f98b65dd
raffyenriquez <30461868+raffyenriquez@users.noreply.github.com>
2018-01-22 14:02:26 +0800

    Update commonCharacterCount.py

--- codefights/Arcade/commonCharacterCount.py
+++ codefights/Arcade/commonCharacterCount.py
@@ -1,10 +1,3 @@
 def commonCharacterCount(s1, s2):
     """returns number of common characters between two strings"""
-    common = 0
-    for char1 in s1:
-        for char2 in s2:
-            if char1 == char2:
-                s2 = s2.replace(char2, "", 1)
-                common += 1
-                break
-    return common
+    return sum(min(s1.count(x),s2.count(x)) for x in set(s1)) 
