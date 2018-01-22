8809693a0aae94f4f59c15253dd7d769a299b4d9
raffyenriquez <30461868+raffyenriquez@users.noreply.github.com>
2018-01-22 13:52:39 +0800

    Update All Longest Strings.py

--- codefights/Arcade/All Longest Strings.py
+++ codefights/Arcade/All Longest Strings.py
@@ -1,8 +1,5 @@
 def allLongestStrings(inputArray):
     """returns an array of strings that contains all the longest strings"""
     longest = max([len(x) for x in inputArray])
-    ls = []
-    for i in inputArray:
-        if len(i) == longest:
-            ls.append(i)
+    ls = [i for i in inputArray if len(i) == longest]
     return ls
