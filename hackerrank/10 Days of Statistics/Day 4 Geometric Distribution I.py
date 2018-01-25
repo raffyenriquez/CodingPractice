8f5aca6a5a07aff2492a9eb4b7a0155876f62919
raffyenriquez <30461868+raffyenriquez@users.noreply.github.com>
2018-01-25 14:12:51 +0800

    Update Day 4 Geometric Distribution I.py

--- hackerrank/10 Days of Statistics/Day 4 Geometric Distribution I.py
+++ hackerrank/10 Days of Statistics/Day 4 Geometric Distribution I.py
@@ -1,7 +1,8 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
 
-"""the probability that a machine produces a defective product is 1/3. What is the probability that the 1st defecct is found during the 5th inspection?"""
+"""the probability that a machine produces a defective product is 1/3. 
+What is the probability that the 1st defect is found during the 5th inspection?"""
 
 def geo_dist(n,p):
     return ((1-p)**(n-1))*p
