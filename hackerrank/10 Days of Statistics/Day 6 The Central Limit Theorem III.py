e8f006c5fcc1607f0b4367cad005c240cee7eefa
raffyenriquez <30461868+raffyenriquez@users.noreply.github.com>
2018-01-26 09:59:52 +0800

    Create Day 6 The Central Limit Theorem III.py

---
+++ hackerrank/10 Days of Statistics/Day 6 The Central Limit Theorem III.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+"""Compute the interval that covers the middle 95% of the distribution sample mean."""
+
+print 500 - 1.96 * (80/100**.5)
+print 500 + 1.96 * (80/100**.5)
